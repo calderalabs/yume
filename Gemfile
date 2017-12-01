ad40f3b896e2e399c33d938ee78d7d3d24807669
Matteo Depalo <matteodepalo@gmail.com>
2017-12-01 11:27:53 +0100

    Add fog gem to handle s3 buckets

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'activeadmin'
 gem 'devise'
 gem 'carrierwave'
 gem 'mini_magick'
+gem 'fog-aws'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,14 @@
 CarrierWave.configure do |config|
-  if Rails.env.production?
+  if ENV['AWS_ACCESS_KEY_ID'].present?
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_S3_REGION']
+      region: 'eu-west-1'
     }
     config.storage = :fog
-    config.fog_directory = ENV['AWS_S3_BUCKET_NAME']
+    config.fog_directory = 'yume-production'
     config.fog_authenticated_url_expiration = 120
     config.fog_attributes = { 'Cache-Control' => "max-age=#{365.days.to_i}" }
   else
